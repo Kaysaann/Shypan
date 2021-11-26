092378854f3dd8a594bc44e6eb2f79ebd7f73c62
Shyba <86374224+Shybaaaaa@users.noreply.github.com>
2021-11-27 00:20:07 +0100

    ReUpload

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="shypan",
+    version="0.0.1.2",
+    author="ShypanLib",
+    author_email="shypanlib@gmail.com",
+    description="Librairie Shypan",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ShypanLib/Shypan",
+    project_urls={
+        "Bug Tracker": "https://github.com/ShypanLib/Shypan/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "shypan"},
+    packages=setuptools.find_packages(where="shypan"),
+    python_requires=">=3.10",
+)
